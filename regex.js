91af847c09678bcca385199b21a7b3c8ad6f1d17
elisa de casas <elisadecasas1@gmail.com>
2022-08-14 12:14:36 -0700

    added email regex, phone number and application status

--- regex.js
+++ regex.js
@@ -31,7 +31,8 @@ Expected Output:
 
 */ 
  
-let phoneReg = /write your regex here/
+let phoneReg = /\d{3}-\d{3}-\d{4}/g
+let phoneArray = sampleApp.match(phoneReg)
 
 /* 
 
@@ -51,7 +52,8 @@ Expected Output:
 
 */ 
 
-let emailReg = /write your regex here/
+let emailReg = /\w+@[a-zA-Z]+.[a-zA-Z]+/
+let emailArray = sampleApp.match(emailReg)
 
 /* 
 
@@ -67,4 +69,5 @@ Expected Output:
 
 */ 
 
-let statusReg = /write your regex here/
+let statusReg = /status:/i
+let statusIndex = sampleApp.search(statusReg)
